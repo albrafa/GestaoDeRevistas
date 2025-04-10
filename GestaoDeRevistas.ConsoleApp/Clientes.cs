9da84e356057464a015c72155c8dedb0dc0eed10
Rafael Albuquerque <rafael.albuquerquev@gmail.com>
2025-04-10 10:43:55 -0300

    Add project files.

---
+++ GestaoDeRevistas.ConsoleApp/Clientes.cs
@@ -0,0 +1,11 @@
+﻿namespace GestaoDeRevistas.ConsoleApp
+{
+    public class ListaClientes
+    {
+        public int Id;
+        public string Nome;
+        public string NomeResponsavel;
+        public string Telefone;        
+
+    }
+}
---
+++ GestaoDeRevistas.ConsoleApp/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace GestaoDeRevistas.ConsoleApp
+{
+    internal class Program
+    {
+
+        public ListaClientes[] listaClientes = new ListaClientes[100];
+        public int contadorCliente = 0;
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----------------");
+            Console.WriteLine("CLUBE DE LEITURA");
+            Console.WriteLine("----------------\n");
+
+
+            Console.WriteLine("Escolha uma opção:");
+            Console.WriteLine("1 - Cadastrar novo cliente: ");
+            Console.WriteLine("4 - Visualizar cliente: ");
+            Console.WriteLine();
+
+            int opcaoEscolhida = Convert.ToInt32(Console.ReadLine());
+
+
+            switch(opcaoEscolhida)
+            {
+                case 1:
+                    Console.Clear();
+                    Console.WriteLine("----------------------");
+                    Console.WriteLine("CLUBE DE LEITURA");
+                    Console.WriteLine("----------------------");
+                    Console.WriteLine();
+
+                    Console.WriteLine("Cadastrando cliente...");
+                    Console.WriteLine("--------------------------");
+                    Console.WriteLine();
+                    Console.Write("Digite o nome do cliente (mínimo 3 caracteres): ");
+                    string nome = Console.ReadLine();
+                    Console.WriteLine();
+
+                    Console.Write("Digite o nome do responsável (mínimo 3 carcteres): ");
+                    Console.ReadLine();
+                    Console.WriteLine();
+
+                    Console.Write("Telefone para contato ((XX) XXXX-XXXX ou (XX) XXXXX-XXXX): ");
+                    Console.ReadLine();
+
+                    break;
+            }
+
+            Console.ReadLine();
+        }
+
+    }
+}
